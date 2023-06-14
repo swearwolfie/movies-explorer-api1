21c2d5c66bc20adeea3953d46b77d77d67aa17b9
Masha Soldatova <threebadwolves@gmail.com>
2023-06-14 03:20:38 +0300

    feat: add model movie

---
+++ models/movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+// const validator = require('validator');
+
+const cardSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String, // ссылка — это строка
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/.test(v);
+      },
+      message: 'Некорректная ссылка',
+    },
+    required: true,
+  },
+  trailerLink: {
+    type: String, // ссылка — это строка
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/.test(v);
+      },
+      message: 'Некорректная ссылка',
+    },
+    required: true,
+  },
+  thumbnail: {
+    type: String, // ссылка — это строка
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/.test(v);
+      },
+      message: 'Некорректная ссылка',
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', cardSchema);
