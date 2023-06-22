e08a000287cb5f48d15d1977ade375b9ae639f35
Masha Soldatova <threebadwolves@gmail.com>
2023-06-22 22:56:41 +0300

    feat: jwt try

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
 }; */
 
 const { JWT_SECRET = 'some-secret-key' } = process.env;
-const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { DB_ADDRESS = 'mongodb://0.0.0.0:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   JWT_SECRET,
