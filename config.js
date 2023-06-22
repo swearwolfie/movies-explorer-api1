397091b162f5ac9cd2d6ff009185be2e7907b51c
Masha Soldatova <threebadwolves@gmail.com>
2023-06-22 23:17:27 +0300

    feat: jwt try ii

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-/* require('dotenv').config();
+require('dotenv').config();
 
 const {
   JWT_SECRET = 'JWT_SECRET',
-  DB_ADDRESS = 'DB_ADDRESS', // mongodb://localhost:27017/bitfilmsdb
+  DB_ADDRESS = 'DB_ADDRESS', // mongodb://127.0.0.1:27017/bitfilmsdb'
   NODE_ENV = 'NODE_ENV',
 } = process.env;
 
@@ -10,12 +10,13 @@ module.exports = {
   JWT_SECRET,
   DB_ADDRESS,
   NODE_ENV,
-}; */
+};
 
+/*
 const { JWT_SECRET = 'some-secret-key' } = process.env;
-const { DB_ADDRESS = 'mongodb://0.0.0.0:27017/bitfilmsdb' } = process.env;
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   JWT_SECRET,
   DB_ADDRESS,
-};
+}; */
